2c399b1798f78942aca3c1deeef23a0271086c5c
Andrea Bona <andrea.bona1996@gmail.com>
2023-11-02 15:51:33 +0100

    Removed WebGL version from the repo.

--- Soccorso-Alpino-Simulator-2.0WebGL/Assets/Scripts/Game_Values/GameValues.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameValues {
-    public static int Score;
-    public static int Scenery;
-    public static int GameMode;
-}
